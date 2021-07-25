14888ee02fba7f79687e482e5c325ffb1e8ecf61
Abhay Patil <abhaypatil87@gmail.com>
2021-07-24 18:18:17 -0600

    logger

--- src/database/index.ts
+++ src/database/index.ts
@@ -121,6 +121,7 @@ class Database {
   }
 
   private async connectAndInitialise() {
+    console.log("Inside connectAndInitialise");
     const isProduction = env === "production";
     const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
     const connectionConfig = {
