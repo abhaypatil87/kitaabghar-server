58309553f91c3cdba383fe2c30fdc41687dd6b3d
Abhay Patil <abhaypatil87@gmail.com>
2021-07-24 18:31:09 -0600

    logger

--- src/database/index.ts
+++ src/database/index.ts
@@ -121,14 +121,13 @@ class Database {
   }
 
   private async connectAndInitialise() {
-    console.log("Inside connectAndInitialise...");
     const isProduction = env === "production";
     const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
     const connectionConfig = {
       connectionString: isProduction
         ? process.env.DATABASE_URL
         : connectionString,
-      ssl: isProduction,
+      ssl: isProduction ? { rejectUnauthorized: false } : false,
     };
 
     const client = new pg.Client(connectionConfig);
