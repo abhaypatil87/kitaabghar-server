432c39527c9cd56dec8a180a2bf300571c26b308
Abhay Patil <abhaypatil87@gmail.com>
2021-07-31 21:26:26 -0600

    Initial commit

--- src/database/migrations/1_1.sql
+++ src/database/migrations/1_1.sql
@@ -1,28 +1,3 @@
-CREATE TABLE IF NOT EXISTS authors
-(
-    author_id     SERIAL PRIMARY KEY,
-    first_name     varchar(50) NOT NULL,
-    last_name      varchar(50) NOT NULL,
-    last_modified TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    created       TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);;
-
-CREATE TABLE IF NOT EXISTS books
-(
-    book_id       SERIAL PRIMARY KEY,
-    title         varchar(150) NOT NULL,
-    subtitle      varchar(150)             DEFAULT NULL,
-    description   varchar(2000)            DEFAULT NULL,
-    isbn_10       varchar(10)              DEFAULT NULL,
-    isbn_13       varchar(13)              DEFAULT NULL,
-    page_count    int                      DEFAULT NULL,
-    author_id     int                      DEFAULT NULL,
-    thumbnail_url varchar(255)             DEFAULT NULL,
-    last_modified TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    created       TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (author_id) REFERENCES authors
-);;
-
 CREATE TABLE IF NOT EXISTS database_version
 (
     version_id  SERIAL PRIMARY KEY,
@@ -30,30 +5,40 @@ CREATE TABLE IF NOT EXISTS database_version
     created     timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP
 );;
 
-CREATE OR REPLACE FUNCTION trigger_set_timestamp() RETURNS TRIGGER AS
-$$
-BEGIN
-    NEW.last_modified = CURRENT_TIMESTAMP;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;;
+CREATE TABLE IF NOT EXISTS externals
+(
+    external_id     SERIAL PRIMARY KEY,
+    external_type   varchar(50) NOT NULL,
+    last_modified   TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    created         TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);;
 
-DROP TRIGGER IF EXISTS authors_last_modified_trig
-  ON authors;;
-CREATE TRIGGER authors_last_modified_trig
-    BEFORE UPDATE
-    ON authors
-    FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();;
+CREATE TABLE IF NOT EXISTS users
+(
+    user_id         SERIAL PRIMARY KEY,
+    first_name      varchar(50) NOT NULL,
+    last_name       varchar(100) NOT NULL,
+    email           varchar(200) NOT NULL,
+    external_id     int DEFAULT NULL,
+    password        varchar(1024),
+    image_url       varchar(1024) DEFAULT NULL,
+    last_modified   TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    created         TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (external_id) REFERENCES externals
+);;
 
+CREATE TABLE IF NOT EXISTS login_history
+(
+    user_id         INTEGER NOT NULL,
+    external_id     INTEGER NOT NULL,
+    ip_address      VARCHAR(64) NOT NULL,
+    created         TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (external_id) REFERENCES externals(external_id)
+);;
 
-DROP TRIGGER IF EXISTS books_last_modified_trig
-  ON books;;
-CREATE TRIGGER books_last_modified_trig
-    BEFORE UPDATE
-    ON books
-    FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_timestamp();;
+INSERT INTO externals(external_type)
+values ('GOOGLE');;
 
 INSERT INTO database_version(script_name)
 VALUES ('1_1.sql');;
